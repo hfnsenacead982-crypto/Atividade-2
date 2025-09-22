248c036046088703df51d0bf1c466755ac021571
hfnsenacead982-crypto <hfnsenacead982@gmail.com>
2025-09-22 02:22:43 -0300

    Implementada funcionalidade de salvar item no banco

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -23,11 +16,27 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        conn = new conectaDAO().connectDB();
+String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+
+try {
+    prep = conn.prepareStatement(sql);
+    prep.setString(1, produto.getNome());
+    prep.setInt(2, produto.getValor());
+    prep.setString(3, produto.getStatus());
+    prep.execute();
+    prep.close();
+} catch (SQLException e) {
+    System.out.println("Erro ao cadastrar: " + e.getMessage());
+} finally {
+    try {
+        if (conn != null) {
+            conn.close();
+        }
+    } catch (SQLException e) {
+        System.out.println("Erro ao fechar conexão: " + e.getMessage());
+    }
+}
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
